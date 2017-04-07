e03c01b099f7dbc8f424e98ed5d894448efc93e1
Seth Ringling <sringling@avvo.com>
2017-04-07 15:49:49 -0700

    Show exceptions in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  # This is a tool for devs... make life easy for yourself and show exceptions
+  config.consider_all_requests_local       = true
+
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
