db27f08f1dc9a346603246b373ff5cd1a5691b35
Seth Ringling <sringling@avvo.com>
2017-04-07 10:51:59 -0700

    configure redis server & client

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,4 +4,8 @@ Sidekiq.configure_client do |config|
   config.redis = redis_config
 end
 
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+end
+
 Redis.current = Sidekiq.redis {|c| c}
