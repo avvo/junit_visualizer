8e5e2ca5fb165e98b9351b610be8cac94f977d30
Seth Ringling <sringling@avvo.com>
2017-04-07 11:02:52 -0700

    add controller for build

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :builds
 
   resource :results do
     post :pull_results
