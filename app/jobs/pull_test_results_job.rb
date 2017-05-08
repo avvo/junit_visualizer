71fea2d018615494a91c263a475362bc97a7e69c
Seth Ringling <sringling@avvo.com>
2017-05-08 14:28:39 -0700

    Create projects by the buckets automatically

--- app/jobs/pull_test_results_job.rb
+++ app/jobs/pull_test_results_job.rb
@@ -3,6 +3,8 @@ class PullTestResultsJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
+    Project.create_from_s3_dir
+
     Project.all.each do |project|
       p "pulling results for #{project.name}"
       project.refresh_project_and_pull_results
--- app/models/project.rb
+++ app/models/project.rb
@@ -15,6 +15,12 @@ class Project < ApplicationRecord
     @s3Wrapper ||= S3Wrapper.new
   end
 
+  def self.create_from_s3_dir
+    S3Wrapper.new.project_names.each do |project_name|
+      Project.find_or_create_by!(name: project_name)
+    end
+  end
+
   def refresh_project_and_pull_results
     process_suites
     process_builds_and_pull_results
